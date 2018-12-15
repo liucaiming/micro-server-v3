c7501dca0db10cebf556f99b638b6eecf544e250
lizibo <21212>
2018-12-15 23:02:30 +0800

    服务治理

---
+++ micro-eureka/src/main/java/com/lcm/it/v2/cloud/EurekaServerApp.java
@@ -0,0 +1,17 @@
+package com.lcm.it.v2.cloud;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer//EurekaServer服务器端启动类,接受其它微服务注册进来
+public class EurekaServerApp {
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(EurekaServerApp.class, args);
+    }
+
+}
--- micro-provider-dept/src/main/java/com/lcm/it/v2/cloud/DeptProviderApp.java
+++ micro-provider-dept/src/main/java/com/lcm/it/v2/cloud/DeptProviderApp.java
@@ -2,8 +2,10 @@ package com.lcm.it.v2.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient //本服务启动后会自动注册进eureka服务中
 public class DeptProviderApp {
 
     public static void main(String[] args) {
